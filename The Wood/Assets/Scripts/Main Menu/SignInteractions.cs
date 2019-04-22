9b738cfb64eee6c92d00baae1a890d045f31339b
Roman Calderon <romanc647@gmail.com>
2019-04-22 18:08:52 -0400

    First prototype of sign interaction
    The signs have rope objects attached to each other and physics can be applied to them. In the SignInteractions class, the player can hover over and click on the signs, apllying a small force vector to their rigidbodies accordingly.

---
+++ The Wood/Assets/Scripts/Main Menu/SignInteractions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignInteractions : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] LayerMask signLayerMask;
+    Transform currentSign;
+    [SerializeField] float hoverForce = 250;
+    [SerializeField] float clickForce = 400;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 25, signLayerMask))
+        {
+            if (currentSign != hit.transform)
+            {
+                currentSign = hit.transform;
+
+                // Apply a small force
+                hit.transform.GetComponent<Rigidbody>().AddForce(Vector3.forward * hoverForce);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 25, signLayerMask))
+            {
+                // Apply a small force
+                hit.transform.GetComponent<Rigidbody>().AddForce(Vector3.forward * clickForce);
+
+                if (hit.transform.tag == "NewGameSign")
+                    print(hit.transform.tag);
+                else if (hit.transform.tag == "ContinueSign")
+                    print(hit.transform.tag);
+                else if (hit.transform.tag == "OptionsSign")
+                    print(hit.transform.tag);
+                else if (hit.transform.tag == "QuitSign")
+                    print(hit.transform.tag);
+            }
+        }
+    }
+}
