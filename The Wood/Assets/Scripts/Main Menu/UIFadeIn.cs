528d0fae34e034b4a4b19b62d2bd4cd8d93db9f8
Roman Calderon <romanc647@gmail.com>
2019-04-27 04:30:09 -0400

    Game Title
    Added a game title with a fade-in effect when the game starts.

---
+++ The Wood/Assets/Scripts/Main Menu/UIFadeIn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class UIFadeIn : MonoBehaviour
+{
+    CanvasGroup canvasGroup;
+
+    [SerializeField] float fadeDelay;
+    [SerializeField] float fadeSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+
+        StartCoroutine(FadeInDelay());
+    }
+
+    IEnumerator FadeInDelay()
+    {
+        yield return new WaitForSeconds(fadeDelay);
+
+        while(canvasGroup.alpha < 1f)
+        {
+            canvasGroup.alpha += Time.deltaTime * fadeSpeed;
+            yield return null;
+        }
+    }
+}
