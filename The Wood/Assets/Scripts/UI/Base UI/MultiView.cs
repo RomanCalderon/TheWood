44ec924891a95fc9e9996f5f1e09540acbb50665
Roman Calderon <romanc647@gmail.com>
2019-04-30 14:01:14 -0400

    Created a Template of UI elements for repurposing
    Created a set of similar-themed UI elements that can be reused. This will be helpful when making UI-heavy interfaces. (Options menu, etc.)
    
    Elements created:
    - Button
    - Header
    - Label Text
    - Multi-View
    - Panel
    - Title Text
    
    Note: Started creating an Options menu using these base UI elements.

---
+++ The Wood/Assets/Scripts/UI/Base UI/MultiView.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class MultiView : MonoBehaviour
+{
+    [SerializeField] Transform tabsHolder;
+    List<Button> tabs = new List<Button>();
+    [SerializeField] Transform viewsHolder;
+    List<Transform> views = new List<Transform>();
+    
+    private void Awake()
+    {
+        foreach (Transform t in tabsHolder)
+        {
+            Button tab = t.GetComponent<Button>();
+
+            if (tab != null)
+                tabs.Add(t.GetComponent<Button>());
+            else
+                continue;
+
+            tab.onClick.AddListener(delegate { OnClick(tabs.IndexOf(tab)); });
+        }
+
+        foreach (Transform t in viewsHolder)
+            views.Add(t);
+    }
+
+    void OnClick(int index)
+    {
+        tabs.ForEach(t => t.interactable = true);
+        tabs[index].interactable = false;
+
+        views.ForEach(v => v.gameObject.SetActive(false));
+        views[index].gameObject.SetActive(true);
+    }
+}
